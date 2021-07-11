3929e33cc72d4e382eac04da04bd375acf42dc31
Kamil Komarowski <k.w.komarowski@gmail.com>
2021-07-11 11:54:56 +0200

    Set up application basic architecture

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import styled from 'styled-components';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import Chat from './components/Chat';
+import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <Router>
+        <Header />
+        <Main>
+          <Sidebar />
+          <Switch>
+            <Route path="/room" component={Chat} />
+            <Route path="/" component={Login} exact />
+            <Route>Not Found</Route>
+          </Switch>
+        </Main>
+      </Router>
+    </AppContainer>
   );
 }
 
 export default App;
+
+const AppContainer = styled.div`
+  min-height: 100vh;
+  display: grid;
+  grid-template-rows: 40px 1fr;
+`;
+
+const Main = styled.div`
+  display: grid;
+  grid-template-columns: 260px 1fr;
+`;
---
+++ src/components/Chat.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+function Chat() {
+  return <div style={{ backgroundColor: 'pink' }}>Chat</div>;
+}
+
+export default Chat;
