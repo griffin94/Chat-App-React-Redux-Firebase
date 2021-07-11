8ee4f5fbfa8ba88e0382b8853e8bbf8e4683f122
Kamil Komarowski <k.w.komarowski@gmail.com>
2021-07-11 11:54:56 +0200

    Set up application basic architecture

---
+++ src/components/Login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+function Login() {
+  return <div>Login</div>;
+}
+
+export default Login;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Sidebar() {
+  return <aside style={{ backgroundColor: 'orange' }}>Sidebar</aside>;
+}
+
+export default Sidebar;
