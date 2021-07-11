ef10918b3392c4fdb84b443a4ccf04761abf824f
Kamil Komarowski <k.w.komarowski@gmail.com>
2021-07-11 19:58:35 +0200

    Getting rooms collection from database

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import { useEffect, useState } from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import database from './firebase/firebase';
 import styled from 'styled-components';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
@@ -6,12 +8,30 @@ import Chat from './components/Chat';
 import Login from './components/Login';
 
 function App() {
+  const [rooms, setRooms] = useState([]);
+
+  useEffect(() => {
+    getChannels();
+  }, []);
+
+  const getChannels = () => {
+    database.collection('rooms').onSnapshot((snapshot) => {
+      const rooms = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        name: doc.data().name,
+      }));
+      setRooms(rooms);
+    });
+  };
+
+  console.log(rooms);
+
   return (
     <AppContainer>
       <Router>
         <Header />
         <Main>
-          <Sidebar />
+          <Sidebar rooms={rooms} />
           <Switch>
             <Route path="/room" component={Chat} />
             <Route path="/" component={Login} exact />
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,58 @@
 import React from 'react';
+import styled from 'styled-components';
+import AddIcon from '@material-ui/icons/AddCircleOutlineRounded';
 
-function Sidebar() {
-  return <aside style={{ backgroundColor: 'orange' }}>Sidebar</aside>;
+function Sidebar({ rooms }) {
+  console.log(rooms);
+  return (
+    <Container>
+      <ChannelsContainer>
+        <NewChannelContainer>
+          <Label>Channels</Label>
+          <Button>
+            <AddIcon />
+          </Button>
+        </NewChannelContainer>
+        <ChannelList>
+          {rooms.map((room) => (
+            <Channel key={room.id}># {room.name}</Channel>
+          ))}
+        </ChannelList>
+      </ChannelsContainer>
+    </Container>
+  );
 }
 
 export default Sidebar;
+
+const Container = styled.aside`
+  background: orange;
+`;
+const ChannelsContainer = styled.div``;
+const NewChannelContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 6px;
+`;
+const Label = styled.span`
+  font-weight: bold;
+`;
+
+const Button = styled.button`
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ChannelList = styled.ul``;
+const Channel = styled.li`
+  cursor: pointer;
+  padding: 6px;
+  &:hover {
+    background: gray;
+  }
+`;
